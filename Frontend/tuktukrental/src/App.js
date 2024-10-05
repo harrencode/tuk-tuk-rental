b352d170567a3482e0b1eba49603b416e1da4611
Harendra Kumarasiri <harendra98w@gmail.com>
2024-10-05 19:06:22 +0530

    Your commit message

---
+++ Frontend/tuktukrental/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to tuk tuk rental App</h1>
+        <button className="start-button">Start</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
